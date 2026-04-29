a8c40c601f2271b8ec7746b88c0e116d3eef33fb
PEPE00157 <qwepepe1234@gmail.com>
2026-04-29 23:02:22 +0700

    SVK

---
+++ SVK/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1777478513,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/namesvk-sheet0.png",
+		"images/play-sheet0.png",
+		"images/help-sheet0.png",
+		"images/ตั้งค่า-sheet0.png",
+		"images/beta-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/x-sheet0.png",
+		"media/กด.ogg",
+		"media/เสียงเกม.ogg",
+		"media/แพ้.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
